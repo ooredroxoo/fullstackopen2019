8e7e789d4d29a7adfaec4bb678b476f5fc79db01
Rafael Nascimento Sampaio <ooredroxoo@gmail.com>
2019-06-14 03:06:13 -0300

    Add assignment 1.1 solution

---
+++ part1/app/src/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Content = (props) => {
+    return (
+        <p>{props.part} {props.exercise}</p>
+    )
+}
+
+export default Content;
---
+++ part1/app/src/Total.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.total}</p>
+    )
+}
+
+export default Total;
